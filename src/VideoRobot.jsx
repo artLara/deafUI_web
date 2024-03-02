5a54b7af2e5f5d61a3b0557e2f362e945be8088b
artLara <art.lara.caz.com>
2024-03-02 00:35:47 -0600

    fix: prototype using videos is working

--- src/VideoRobot.jsx
+++ src/VideoRobot.jsx
@@ -5,7 +5,7 @@ export default function VideoRobot() {
     return (
       <div>
         <video width="auto" height="auto" autoPlay muted>
-            <source src={"../videos_robot/amar.mp4"} type="video/ogg"/>
+            <source src={"../videos_robot/message1.mp4"} type="video/ogg"/>
             Your browser does not support the video tag.
         </video>
       </div>
